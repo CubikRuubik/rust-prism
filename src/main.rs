43dfce745a856d71c30536a0bb91ce2f5cd5dce8
Oleh <cuubikruubik@gmail.com>
2026-04-14 13:03:01 +0300

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-mod queue;
-
-use queue::Queue;
-
 fn main() {
-    let mut q = Queue::new();
-    q.enqueue(10);
-    q.enqueue(20);
-    q.enqueue(30);
-
-    println!("Queue size: {}", q.size());
-
-    for item in q.drain() {
-        println!("ID: {}, Value: {}", item.id, item.value);
-    }
-
-    println!("Queue size after drain: {}", q.size());
+    println!("Hello, world!");
 }
